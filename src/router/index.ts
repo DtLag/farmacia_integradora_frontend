1dfd9f702a4343c6ef072d8b3a809fc44e8c91a5
cinthiayocelynavinarivera <24170032@uttcampus.edu.mx>
2026-03-24 11:43:29 -0600

    feat(auth,createFetch): add useApiFetch, authStore and a provisional login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ProvisionalLogin from '@/views/ProvisionalLogin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: ProvisionalLogin,
+    },
+  ],
 })
 
 export default router
