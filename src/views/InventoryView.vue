574a67045ba1f091f44c5fa5fc6f4a6399a25b4c
Anibal <anibalhernandez787@gmail.com>
2026-04-17 01:53:29 -0600

    Fix API_BASE_URL

--- src/views/InventoryView.vue
+++ src/views/InventoryView.vue
@@ -8,7 +8,7 @@ import { useApi } from '@/composables/useApiFetch'
 import { usePublicApi } from '@/composables/usePublicApi'
 import { useAuthStore } from '@/stores/auth'
 
-const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL ?? 'http://127.0.0.1:8000/api').replace(/\/+$/, '')
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 function apiUrl(path: string) {
   return `${API_BASE_URL}/${path.replace(/^\/+/, '')}`
